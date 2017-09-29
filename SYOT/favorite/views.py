e71bf618a502ff391d9684e7ec24d77cc2e79909
baivarn-tjr <t.sukkree@hotmail.com>
2017-09-29 18:01:07 +0700

    enable favorite page

--- SYOT/favorite/views.py
+++ SYOT/favorite/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def favorite(request):
+    context = locals()
+    template = 'favorite/favorite.html'
+    return render(request, template , context)
