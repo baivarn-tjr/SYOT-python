a0cde2373673a8fb214712c092c3c892ac299f60
nkbss <kanoktat-b@hotmail.com>
2017-10-01 14:48:14 +0700

    modal product-page

---
+++ static/static_root/js/product-page.js
@@ -0,0 +1,48 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('front-model');
+var subImg1 = document.getElementById('sub-model-1')
+var subImg2 = document.getElementById('sub-model-2')
+var subImg3 = document.getElementById('sub-model-3')
+var subImg4 = document.getElementById('sub-model-4')
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+subImg1.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+subImg2.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+subImg3.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+subImg4.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
