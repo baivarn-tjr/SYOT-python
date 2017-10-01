15c185b00fdf2d2c4f52c5becef5a851dbf36bba
baivarn-tjr <t.sukkree@hotmail.com>
2017-10-01 18:52:41 +0700

    able to delete from carts

--- SYOT/carts/urls.py
+++ SYOT/carts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # ex: /polls/
     url(r'^$', views.cart, name='cart'),
     url(r'^addminusQuantity/(?P<user_id>[0-9]+)/(?P<product_id>[0-9]+)/$', views.addminusQuantity, name='addminusQuantity'),
+    url(r'^delete/(?P<user_id>[0-9]+)/(?P<product_id>[0-9]+)/$', views.delete, name='delete'),
 ]
--- SYOT/carts/views.py
+++ SYOT/carts/views.py
@@ -17,12 +17,12 @@ def calShipping(userBasket,usercart,carts):
     ship = 5
     for item in usercart:
         itemCart = Basket.objects.get(productID = item, userId = userBasket)
-        ship += 3
+        ship += (3*itemCart.quantity)
     ship -= 3
     return ship
 
 def cart(request):
-    userBasket = User.objects.get(username="varn")
+    userBasket = User.objects.get(id=1)
     usercart = userBasket.myBasket.all()
     carts = Basket.objects.all()
     money = calMoney(userBasket,usercart,carts)
@@ -37,6 +37,26 @@ def cart(request):
     template = 'carts/basket.html'
     return render(request,template,context)
 
+def delete(request, user_id, product_id):
+    user = User.objects.get(id=user_id)
+    product = Product.objects.get(id=product_id)
+    item = get_object_or_404(Basket, userId=user, productID=product)
+    item.delete()
+    
+    userBasket = User.objects.get(id=1)
+    usercart = userBasket.myBasket.all()
+    carts = Basket.objects.all()
+    money = calMoney(userBasket,usercart,carts)
+    ship = calShipping(userBasket,usercart,carts)
+    context = {
+        'user' : userBasket,
+        'usercart' : usercart,
+        'carts' : carts,
+        'money' : money,
+        'ship' : ship,
+    }
+    template = 'carts/basket.html'
+    return render(request,template,context)
 
 def addminusQuantity(request, user_id , product_id):
     user = User.objects.get(id=user_id)
@@ -49,7 +69,7 @@ def addminusQuantity(request, user_id , product_id):
         item.quantity -= 1
     item.save()
 
-    userBasket = User.objects.get(username="varn")
+    userBasket = User.objects.get(id=1)
     usercart = userBasket.myBasket.all()
     carts = Basket.objects.all()
     money = calMoney(userBasket,usercart,carts)
