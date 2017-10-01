9ed064a8b8e76e5aed3e356a2daf3930731a242b
baivarn-tjr <t.sukkree@hotmail.com>
2017-10-01 18:36:22 +0700

    add minus quantity

--- SYOT/carts/views.py
+++ SYOT/carts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse
 
@@ -38,5 +38,28 @@ def cart(request):
     return render(request,template,context)
 
 
+def addminusQuantity(request, user_id , product_id):
+    user = User.objects.get(id=user_id)
+    product = Product.objects.get(id=product_id)
+    item = get_object_or_404(Basket, userId=user, productID=product)
 
-# def
+    if 'inc' in request.POST:
+        item.quantity += 1
+    else:
+        item.quantity -= 1
+    item.save()
+
+    userBasket = User.objects.get(username="varn")
+    usercart = userBasket.myBasket.all()
+    carts = Basket.objects.all()
+    money = calMoney(userBasket,usercart,carts)
+    ship = calShipping(userBasket,usercart,carts)
+    context = {
+        'user' : userBasket,
+        'usercart' : usercart,
+        'carts' : carts,
+        'money' : money,
+        'ship' : ship,
+    }
+    template = 'carts/basket.html'
+    return render(request,template,context)
